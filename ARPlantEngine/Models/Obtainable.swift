28a1fa8de1741144347a0ffb753fe08cbce95066
Gagatek5 <tomek.twa@me.com>
2018-07-11 18:40:38 +0200

    first commit

---
+++ ARPlantEngine/Models/Obtainable.swift
@@ -0,0 +1,9 @@
+//
+//  Obtainable.swift
+//  ARPlantEngine
+//
+//  Created by Dave Szczutkowski on 16/06/2018.
+//  Copyright © 2018 D&T Entertainment. All rights reserved.
+//
+
+import Foundation
